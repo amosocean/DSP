7aba31b54bb092cd8aebe86a59c22467376f5cbb
Hao Haiyang <506474841@qq.com>
2021-05-04 11:24:37 +0800

    m2输出两个频率

--- m2.m
+++ m2.m
@@ -1,9 +1,10 @@
 clear 
-fs=10000;
-f=697;
+fs=8000;
+f1=697;
+f2=1209;
 T=1/fs;
-t=[0:T:f/f-T];
-input=cos(2*pi*f.*t);
+t=[0:T:1-T];
+input=cos(2*pi*f1.*t)+cos(2*pi*f2.*t);
 figure(1);
 stem(t,input);
 output=fftshift(fft(input));
@@ -11,12 +12,13 @@ figure(2);
 stem([0:length(input)-1],abs(output));
 %stem(linspace(-fs/2,fs/2,length(t)),abs(output));
 N=length(input);
-x=zeros(1,N);
-for k=[1:N]
+x_squre=zeros(1,N);
+for k=[600:1800]
 [vk,vk_last]=compute_vk(N,input,N,k);
-x(k)=vk-(exp(-1i*2*pi/N*k))*vk_last;
+x_squre(k)=vk^2+vk_last^2-2*cos(2*pi*k/N)*vk*vk_last;
 end
 figure(3)
-stem(abs(x));
+decode=x_squre>10^6;
+find(decode)
 
 
